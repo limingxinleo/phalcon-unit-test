a2e6cbff835ccca9dee3e16d2ce5957c7ed4676d
李铭昕 <715557344@qq.com>
2018-01-16 16:47:35 +0800

    格式化代码

--- app/controllers/Api/IndexController.php
+++ app/controllers/Api/IndexController.php
@@ -6,13 +6,10 @@ use App\Controllers\Controller;
 
 class IndexController extends Controller
 {
-
     public function indexAction()
     {
         return $this->response->setJsonContent([
             'message' => 'I am IndexController@index'
         ]);
     }
-
 }
-
