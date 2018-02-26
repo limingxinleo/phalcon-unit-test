3a5a3148a17ebad20ba595d1f26bc2e163255523
李铭昕 <715557344@qq.com>
2018-02-26 18:10:32 +0800

    增加Socket Server测试

---
+++ app/tasks/Server/SocketTask.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Tasks\Server;
+
+use App\Core\Cli\Task\Socket;
+use Xin\Cli\Color;
+use Xin\Phalcon\Cli\Traits\Input;
+use swoole_server;
+use swoole_process;
+
+class SocketTask extends Socket
+{
+    use Input;
+
+    public function onConstruct()
+    {
+        $this->port = 11521;
+        $this->host = '127.0.0.1';
+    }
+
+    protected function events()
+    {
+        return [
+            'receive' => [$this, 'receive'],
+            'WorkerStart' => [$this, 'workerStart'],
+        ];
+    }
+
+    protected function beforeServerStart(swoole_server $server)
+    {
+        parent::beforeServerStart($server);
+        $config = $this->getConfig();
+        if ($this->option('daemonize')) {
+            $config['daemonize'] = true;
+        }
+        // 重置参数
+        $server->set($config);
+
+        // 工作进程
+        $worker = new swoole_process(function (swoole_process $worker) use ($server) {
+            swoole_timer_tick(1000, function () use ($worker) {
+                echo 'tick:' . time() . ':' . $worker->pid . PHP_EOL;
+            });
+            $server->tick(1000, function () {
+                echo 'tick:' . time() . PHP_EOL;
+            });
+        });
+
+        $server->addProcess($worker);
+    }
+
+    public function workerStart(swoole_server $server, $workerId)
+    {
+        // 查看不能被平滑重启的文件
+        // $files = get_included_files();
+        // dump($files);
+    }
+
+    public function receive(swoole_server $server, $fd, $reactor_id, $data)
+    {
+        echo $data . PHP_EOL;
+        $server->send($fd, 'receive:' . $data);
+    }
+
+    protected function getConfig()
+    {
+        // @see https://wiki.swoole.com/wiki/page/274.html Swoole文档Socket配置选项
+        $pidsDir = di('config')->application->pidsDir;
+        return [
+            'pid_file' => $pidsDir . 'socket.pid',
+            'user' => 'nginx',
+            'group' => 'nginx',
+            'daemonize' => false,
+            // 'worker_num' => 8, // cpu核数1-4倍比较合理 不写则为cpu核数
+            'max_request' => 500, // 每个worker进程最大处理请求次数
+        ];
+    }
+}
+
---
+++ tests/Swoole/SocketTest.php
@@ -0,0 +1,32 @@
+<?php
+// +----------------------------------------------------------------------
+// | 基础测试类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Units;
+
+use Tests\UnitTestCase;
+use swoole_client;
+
+/**
+ * Class UnitTest
+ */
+class SocketTest extends UnitTestCase
+{
+    public function testSocketSend()
+    {
+        $client = new swoole_client(SWOOLE_TCP | SWOOLE_KEEP);
+        if (!$client->connect('127.0.0.1', 11521, 0.1)) {
+            throw new RpcException("connect failed. Error: {$client->errCode}");
+        }
+
+        $data = 'xxxx';
+        $client->send($data);
+        $res = $client->recv();
+
+        $this->assertEquals('receive:' . $data, $res);
+    }
+}
