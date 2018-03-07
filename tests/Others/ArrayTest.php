dd17e71b22d88cbb7f719b7238940f1839af3508
李铭昕 <715557344@qq.com>
2018-03-07 12:31:03 +0800

    增加部分常用函数单元测试

---
+++ tests/Others/ArrayTest.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | PregTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Others;
+
+use App\Common\Enums\ErrorCode;
+use App\Common\Helper\Match;
+use Tests\UnitTestCase;
+
+/**
+ * Class UnitTest
+ */
+class ArrayTest extends UnitTestCase
+{
+    public function testArrayFlip()
+    {
+        $res = array_flip(['a', 'b', 'c']);
+
+        $this->assertEquals(['a' => 0, 'b' => 1, 'c' => 2], $res);
+    }
+}
---
+++ tests/Others/FileTest.php
@@ -0,0 +1,41 @@
+<?php
+// +----------------------------------------------------------------------
+// | PregTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Others;
+
+use App\Common\Enums\ErrorCode;
+use App\Common\Helper\Match;
+use Tests\UnitTestCase;
+
+/**
+ * Class UnitTest
+ */
+class FileTest extends UnitTestCase
+{
+    public function testFOpen()
+    {
+        $file = ROOT_PATH . '/public/static/file/user.txt';
+        $handler = fopen($file, 'r');
+
+        $result = [];
+        while (!feof($handler)) {
+            $line = fgets($handler);
+            $line = str_replace(PHP_EOL, '', $line);
+            $item = explode(' ', $line);
+            $result[] = $item;
+        }
+
+        $this->assertEquals(
+            [
+                ['limx', 'Agnes'],
+                ['wsh']
+            ],
+            $result
+        );
+    }
+}
