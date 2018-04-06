d191778ebf31945cfeae93d5551bded7074862c6
李铭昕 <715557344@qq.com>
2018-04-06 10:03:34 +0800

    Fix Travis BUG

--- tests/Others/ArrayTest.php
+++ tests/Others/ArrayTest.php
@@ -51,11 +51,14 @@ class ArrayTest extends UnitTestCase
 
     public function testArrayQuote()
     {
-        Quote::getInstance()->getValues()['test'] = 'test';
-        $this->assertEquals([], Quote::getInstance()->getValues());
+        if (version_compare(PHP_VERSION, '7.0', '>=')) {
+            Quote::getInstance()->getValues()['test'] = 'test';
+            $this->assertEquals([], Quote::getInstance()->getValues());
 
-        Quote::getInstance()->getValuesQuote()['test'] = 'test';
-        $this->assertEquals(['test' => 'test'], Quote::getInstance()->getValues());
+            Quote::getInstance()->getValuesQuote()['test'] = 'test';
+            $this->assertEquals(['test' => 'test'], Quote::getInstance()->getValues());
+        }
+        $this->assertTrue(true);
     }
 
     public function testObjectQuote()
