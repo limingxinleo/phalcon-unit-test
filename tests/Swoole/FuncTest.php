65ac1f46f680fb51a580c2ecb920e57e17d901de
李铭昕 <715557344@qq.com>
2018-05-21 16:49:53 +0800

    增加Swoole 方法测试

---
+++ tests/Swoole/FuncTest.php
@@ -0,0 +1,37 @@
+<?php
+// +----------------------------------------------------------------------
+// | 基础测试类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Units;
+
+use App\Common\Clients\Rpc\BasicClient;
+use App\Common\Clients\TestClient;
+use App\Utils\Redis;
+use Phalcon\Text;
+use Tests\UnitTestCase;
+use swoole_client;
+
+/**
+ * Class UnitTest
+ */
+class FuncTest extends UnitTestCase
+{
+    public function testSwooleGetLocalIp()
+    {
+        $this->assertTrue(count(swoole_get_local_ip()) > 0);
+    }
+
+    public function testSwooleCpuNum()
+    {
+        $this->assertTrue(count(swoole_cpu_num()) > 0);
+    }
+
+    public function testSwooleVersion()
+    {
+        $this->assertTrue(version_compare(swoole_version(), '1.0', '>'));
+    }
+}
