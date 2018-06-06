18e681b8c36512ddd9891fcaababee60d0622765
李铭昕 <715557344@qq.com>
2018-06-06 14:31:14 +0800

    增加别名缓存测试

--- tests/Others/ClassTest.php
+++ tests/Others/ClassTest.php
@@ -11,6 +11,7 @@ namespace Tests\Others;
 use App\Biz\Objects\Alias;
 use App\Biz\Objects\Invoke;
 use App\Common\Enums\ErrorCode;
+use Symfony\Component\Finder\Finder;
 use Tests\UnitTestCase;
 use Alias2;
 
@@ -31,6 +32,61 @@ class ClassTest extends UnitTestCase
         $this->assertTrue($o1 instanceof Alias2);
         $this->assertTrue($o2 instanceof Alias);
         $this->assertTrue($o2 instanceof Alias2);
+    }
+
+    public function testClassAliasCache()
+    {
+        $dir = ROOT_PATH . '/libs/aliyun-openapi-php-sdk';
+        require_once $dir . '/aliyun-php-sdk-core/Config.php';
+
+        $btime = microtime(true);
+        $finder = Finder::create()->in($dir);
+
+        $targetClass = [];
+        $iterator = $finder->files()->getIterator();
+        foreach ($iterator as $file) {
+            $classArr = explode('/', $file->getRelativePathname());
+            if (isset($classArr[2]) && $classArr[2] == 'Request') {
+                unset($classArr[0]);
+                $target = rtrim(implode('\\', $classArr), '.php');
+                unset($classArr[3]);
+                $alias = rtrim(implode('\\', $classArr), '.php');
+                if (class_exists($target)) {
+                    class_alias($target, $alias);
+                    $targetClass[$alias] = $target;
+                }
+            }
+        }
+
+        $etime = microtime(true);
+        $time1 = $etime - $btime;
+
+        $request = new \Ecs\Request\V20140526\DescribeAvailableResourceRequest();
+        $this->assertEquals('POST', $request->getMethod());
+
+        $request = new \Ecs\Request\DescribeAvailableResourceRequest();
+        $this->assertEquals('POST', $request->getMethod());
+
+        /** @var \Phalcon\Cache\BackendInterface $cache */
+        $cache = di('cache');
+        $cache->save('aliyun:openapi', $targetClass);
+
+        $btime = microtime(true);
+        $arr = $cache->get('aliyun:openapi');
+        foreach ($arr as $alias => $class) {
+            if (class_exists($class)) {
+                class_alias($class, $alias . '2');
+            }
+        }
+
+        $etime = microtime(true);
+
+        $time2 = $etime - $btime;
+
+        $request = new \Ecs\Request\DescribeAvailableResourceRequest2();
+        $this->assertEquals('POST', $request->getMethod());
+        
+        $this->assertTrue($time2 < $time1);
     }
 
     public function testClassExist()
