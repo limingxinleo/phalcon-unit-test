40fc9071a110c3f838ae2584bb17a95256c1776e
李铭昕 <715557344@qq.com>
2018-06-08 19:10:59 +0800

    更新子进程的子进程处理操作

--- app/common/Server.php
+++ app/common/Server.php
@@ -29,8 +29,12 @@ class Server extends RpcServer
 
             while (true) {
                 $proc = new swoole_process(function (swoole_process $p) {
-                    $task = ROOT_PATH . '/run test:test@addCache';
-                    exec('php ' . $task);
+                    /** @var BackendInterface $cache */
+                    $cache = di('cache');
+                    $cache->save('another:task:save:cache', 'Hi, limx');
+
+                    // $task = ROOT_PATH . '/run test:test@addCache';
+                    // exec('php ' . $task);
                 });
                 $proc->start();
                 sleep(1);
